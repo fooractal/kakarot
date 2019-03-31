c2ec5d04c5c1737388796582f22066af2d3ef224
fooractal <fooractal.mine@gmail.com>
2019-03-31 14:53:33 +0900

    HTM Parserを追加

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 mod dom;
+mod html;
 
 fn main() {
-    let dom_tree = dom::elem(
-        "body".to_string(),
-        dom::AttrMap::new(),
-        vec![
-            dom::elem(
-                "h1".to_string(),
-                dom::AttrMap::new(),
-                vec![dom::text("hello kakarot".to_string())],
-            ),
-            dom::text("hello".to_string()),
-        ],
-    );
+    let source = r#"
+    <html lang="ja">
+      <head>
+        <title>test</title>
+      </head>
+      <body>
+        <h1>parsing test</h1>
+        <p class="greeting" style="color: red">Hello world!</p>
+
+        sample
+      </body>
+    </html>
+"#;
+
+    let dom_tree = html::parse(source.to_string());
     dom::walk_dom_tree(&dom_tree, 0);
 }
